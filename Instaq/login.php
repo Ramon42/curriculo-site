c808bcc3d6745d9f59a9fb58bbbd208dbcf0f824
Ramon Garcia Camargo <33274934+Ramon42@users.noreply.github.com>
2019-05-14 02:33:44 -0300

    Projeto InstaClone

---
+++ Instaq/login.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang=pt dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Insmatch - Registro</title>
+    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+  </head>
+  <body id = "startScreen">
+    <main>
+      <section id="mainSection">
+        <header>
+          <h1><i class="fas fa-camera-retro"></i>InstaMatch</h1>
+          <h3>Entre para ver fotos e vídeos dos seus amigos</h3>
+        </header>
+        <div>
+          <form>
+            <div>
+              <input type="text" name="usuario" placeholder="Nome de usuário">
+            </div>
+            <div>
+              <input type="password" name="senha" placeholder="Senha">
+            </div>
+            <div>
+              <a class="buttons" href="login.php">Entrar</a>
+            </div>
+            <a href="registro.php">Esqueceu a senha?</a>
+          </form>
+        </div>
+      </section>
+      <section id="mainSection">
+        <footer>
+          <h3>Não tem uma conta?</h3>
+          <a class="buttons buttons-2" href="registro.php">Cadastre-se</a>
+        </footer>
+      </section>
+    </main>
+  </body>
+</html>
---
+++ Instaq/registro.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang=pt dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Insmatch - Registro</title>
+    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+  </head>
+  <body id="startScreen">
+    <main>
+      <section id="mainSection">
+        <header>
+          <h1><i class="fas fa-camera-retro"></i> InstaMatch</h1>
+          <h3>Cadastre-se para ver fotos e vídeos dos seus amigos</h3>
+        </header>
+        <div>
+          <form>
+            <div>
+              <input type="text" name="email" placeholder="Número de celular ou e-mail">
+            </div>
+            <div>
+              <input type="text" name="nome" placeholder="Nome completo">
+            </div>
+            <div>
+              <input type="text" name="usuario" placeholder="Nome de usuário">
+            </div>
+            <div>
+              <input type="password" name="senha" placeholder="Senha">
+            </div>
+            <div>
+              <input type="checkbox" name="concordo">
+              <label for="concordo" id="termosText">Concordo com os termos,
+                Políticas de Dados e Políticas de Cookies</label>
+            </div>
+            <div>
+              <a class="buttons" href="login.php">Cadastre-se</a>
+            </div>
+          </form>
+        </div>
+        </section>
+        <footer id="mainSection">
+          <h3>Já tem uma conta?</h3>
+          <a class="buttons buttons-2" href="login.php">Conecte-se</a>
+        </footer>
+    </main>
+  </body>
+</html>
