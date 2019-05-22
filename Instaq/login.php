c2b188143bf88d44d4bb19e5dadb950e70b6e348
Ramon Garcia Camargo <33274934+Ramon42@users.noreply.github.com>
2019-05-22 20:39:22 -0300

    Add files via upload

--- Instaq/login.php
+++ Instaq/login.php
@@ -2,7 +2,8 @@
 <html lang=pt dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>Insmatch - Login</title>
+    <title>Instamatch - Login</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/css/style.css">
     <link href="https://fonts.googleapis.com/css?family=Shadows+Into+Light" rel="stylesheet">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
